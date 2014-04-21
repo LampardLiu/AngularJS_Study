30f1c082ab49dc8e509d3dcfe5631709d36379f7
Lampard <646007589@qq.com>
2014-04-22 00:34:50 +0800

    添加一个httpDemo
    OK

--- demo/js/demo.js
+++ demo/js/demo.js
@@ -6,8 +6,10 @@ demoApp.controller('demoList',['$scope',function($scope) {
   $scope.demoTitle = 'Demo of AngularJS';
   $scope.demoList = [{dis:'双向绑定',url:'../demo/demoHtml/dataBind.html'},
                      {dis:'表单demo',url:'../demo/demoHtml/formDemo.html'},
+                     {dis:'Http数据读取demo',url:'../demo/demoHtml/httpDemo.html'},
                      {dis:'拼图demo',url:'../demo/demoHtml/spellImage.html'}];
 }]).
+//表单demo
 controller('formDemo',['$scope',function($scope) {
   $scope.demoTitle = 'FormDemo';
   $scope.user = {};
@@ -25,10 +27,19 @@ controller('formDemo',['$scope',function($scope) {
   $scope.$watch('user',function() {
     $scope.count ++;
   },true);
-}]).controller('spileImage',['$scope',function($scope) {
+}]).
+//拼图demo
+controller('spileImage',['$scope',function($scope) {
   $scope.demoTitle = 'SpileImage';
   $scope.loadImage = function(elementID) {
     var board = angular.element(document.getElementById(elementID))[0];
     board.setAttribute('style','background:url(\'../img/spellImage_1.png\');width:396px;height:371px;');
   }
+}]).
+controller('dataRead',['$scope','$http',function($scope,$http) {
+  $http.get('../data/data.txt').success(function(data) {
+    $scope.dataList = data;  
+  }).error(function(data) {
+    $scope.dataList = '[{"name":"NODATA"}]';
+  });
 }]);
