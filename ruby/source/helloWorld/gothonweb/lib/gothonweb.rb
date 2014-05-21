8f0bf12d34cc3253a325caab9b779759d41b2c56
Lampard <iml6yu@gmail.com>
2014-05-21 23:14:34 +0800

    需要更多的练习和实操

--- ruby/source/helloWorld/gothonweb/lib/gothonweb.rb
+++ ruby/source/helloWorld/gothonweb/lib/gothonweb.rb
@@ -1,11 +1,22 @@
 require_relative "gothonweb/version"
 require "sinatra"
-
+require "erb"
 #
 #module Gothonweb
-  get '/' do
+get '/' do
     greeting = "Hello, World!"
+    erb :hello_form, :locals => {:greeting => greeting}
+     #   return greeting
+end
+
+get  '/hello' do
+    name = params[:name] || "NoBody"
+    greeting = "Hello #{name}!"
     erb :index, :locals => {:greeting => greeting}
- #   return greeting
-  end
+ end
+
+post '/hello' do
+    greeting = "#{params[:greet] || "Hello"}, #{params[:name] || "Nobody"}"
+    erb :index, :locals => {:greeting => greeting}
+end
 #end
