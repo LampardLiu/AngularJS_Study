1200ebb0d1ccfc8e04ece9e7a7589bd700a5d934
Lampard <646007589@qq.com>
2014-05-27 08:53:59 +0800

    ruby 学习计划

---
+++ web/draw/js/ResourceOperationTool.js
@@ -0,0 +1,14 @@
+﻿/*
+Data:2014-5-26
+Author：Lampard
+Target：处理基础操作
+*/
+
+if (typeof operationTool === 'undefined') {
+    var operationTool = {
+        getString: function () { alert("Hello"); },
+        getName: function () { alert("Lampard"); }
+    }
+}
+
+window.$$ = $$ = operationTool;
